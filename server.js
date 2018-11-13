df869960aaaeaaa93fa90064823bd047e408173a
Codey Worley <codeyworley@gmail.com>
2018-11-13 13:16:22 -0700

    added 404 and 500 error middleware

--- server.js
+++ server.js
@@ -73,6 +73,24 @@ app.get('/api/notes/:id', (req, res) => {
 
 });
 
+app.get('/boom', (req, res, next) => {
+    throw new Error('Boom!!');
+  });
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+
 // Listen for incoming connections
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
