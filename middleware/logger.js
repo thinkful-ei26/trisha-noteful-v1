54a7a3ae0b11a4699e21c8f289296bf8d72abc04
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-14 14:58:38 -0500

    modularized router

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,7 @@
+'use strict';
 function logger(req, res, next) {
-    console.log(Date.now(), req.method, req.url);
-    next();
+  console.log(Date.now(), req.method, req.url);
+  next();
 }
 
 module.exports = {logger};
