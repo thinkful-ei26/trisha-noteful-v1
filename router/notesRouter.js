a44b99827bf42330f79ed3de3f5ba527c23499d6
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-14 15:05:49 -0500

    added post

--- router/notesRouter.js
+++ router/notesRouter.js
@@ -103,4 +103,29 @@ notesRouter.put('/notes/:id', (req, res, next) => {
   });
 });
 
+// Post (insert) an item
+notesRouter.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+
 module.exports = notesRouter;
