593efad7cf4fd8bf14c323fd4a08832075e9d0f5
kronicle114 <trisha.aguinaldo@gmail.com>
2018-11-15 16:36:00 -0500

    noteful api promisify

--- public/scripts/noteful.js
+++ public/scripts/noteful.js
@@ -42,10 +42,17 @@ const noteful = (function () {
 
       const noteId = getNoteIdFromElement(event.currentTarget);
 
-      api.details(noteId, detailsResponse => {
-        store.currentNote = detailsResponse;
-        render();
-      });
+      // api.details(noteId, detailsResponse => {
+      //   store.currentNote = detailsResponse;
+      //   render();
+      // });
+
+
+      api.details(noteId)
+        .then(detailsResponse => {
+          store.currentNote = detailsResponse;
+          render(); 
+        });
 
     });
   }
@@ -57,10 +64,16 @@ const noteful = (function () {
       const searchTerm = $('.js-note-search-entry').val();
       store.currentSearchTerm = searchTerm ? { searchTerm } : {};
 
-      api.search(store.currentSearchTerm, searchResponse => {
-        store.notes = searchResponse;
-        render();
-      });
+      api.search(store.currentSearchTerm)
+        .then(searchResponse => {
+          store.notes = searchResponse;
+          render();
+        });
+
+      // api.search(store.currentSearchTerm, searchResponse => {
+      //   store.notes = searchResponse;
+      //   render();
+      // });
 
     });
   }
@@ -82,10 +95,16 @@ const noteful = (function () {
         api.update(store.currentNote.id, noteObj, updateResponse => {
           store.currentNote = updateResponse;
 
-          api.search(store.currentSearchTerm, searchResponse => {
-            store.notes = searchResponse;
-            render();
-          });
+          api.search(store.currentSearchTerm)
+            .then(searchResponse => {
+              store.notes = searchResponse;
+              render();
+            });
+
+          // api.search(store.currentSearchTerm, searchResponse => {
+          //   store.notes = searchResponse;
+          //   render();
+          // });
 
         });
 
@@ -94,11 +113,16 @@ const noteful = (function () {
         api.create(noteObj, createResponse => {
           store.currentNote = createResponse;
 
-          api.search(store.currentSearchTerm, searchResponse => {
-            store.notes = searchResponse;
-            render();
-          });
+          // api.search(store.currentSearchTerm, searchResponse => {
+          //   store.notes = searchResponse;
+          //   render();
+          // });
 
+          api.search(store.currentSearchTerm)
+            .then(searchResponse => {
+              store.notes = searchResponse;
+              render();
+            });
         });
       }
 
@@ -123,13 +147,24 @@ const noteful = (function () {
 
       api.remove(noteId, () => {
 
-        api.search(store.currentSearchTerm, searchResponse => {
-          store.notes = searchResponse;
-          if (noteId === store.currentNote.id) {
-            store.currentNote = {};
-          }
-          render();
-        });
+
+        api.search(store.currentSearchTerm)
+          .then(searchResponse => {
+            store.notes = searchResponse;
+            if (noteId === store.currentNote.id) {
+              store.currentNote = {};
+            }
+            render();
+          });
+
+        // api.search(store.currentSearchTerm, searchResponse => {
+        //   store.notes = searchResponse;
+        //   if (noteId === store.currentNote.id) {
+        //     store.currentNote = {};
+        //   }
+        //   render();
+        // });
+        
       });
       
     });
